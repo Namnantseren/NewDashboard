432222896d76408b55c1f1c59b9ea5996b743c4e
Morning <morning@admins-iMac.local>
2023-02-10 13:15:32 +0800

    hdoa

--- src/Dashboard/DashPage/Users.jsx
+++ src/Dashboard/DashPage/Users.jsx
@@ -1,24 +1,56 @@
-import Dashboard from "../Dashside/Dashside"
-import Dashboardnav from "../Dnav/Dashboardnav"
-import './DashStyle/DashStyle.css'
+import Dashboard from "../Dashside/Dashside";
+import Dashboardnav from "../Dnav/Dashboardnav";
+import Dropdown from "react-bootstrap/Dropdown";
+import "./DashStyle/DashStyle.css";
 
 export default function Dashdash() {
-    return (
-        <div>
-            <Dashboardnav/>
-            <div className="d-flex">
-                <Dashboard/>
-                <div className="allofdash">
-                    <div className="allDash">
-                        <div className="navofdash">
-                            <h1 className="containerofdd">Хэрэглэгчид</h1>
-                        </div>
-                        <div className="midofdash">
-
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <Dashboardnav />
+      <div className="d-flex">
+        <Dashboard />
+        <div className="allofdash">
+          <div className="allDash">
+            <div className="navofdash">
+              <h1 className="containerofdd">Хэрэглэгчид</h1>
             </div>
+            <div className="d-flex justify-content-between mt-5">
+              <div>
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Dropdown Button
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">
+                      Another action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">
+                      Something else
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+              <div>
+                <input type="Хайх" />
+              </div>
+            </div>
+            <div className="midofdash ">
+              <div className="flex justify-content-around prod-list-color pt-3">
+                <p>User ID</p>
+                <p>Овог</p>
+                <p>Нэр</p>
+                <p>И-мэйл хаяг</p>
+                <p>Утас</p>
+                <p>Захиалга</p>
+                <p>Огноо</p>
+                <p>:</p>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
